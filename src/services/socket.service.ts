7c02befe75ab14a5ed98e363dab6bd5b3faf5394
Brandon <brandonyoyoslade@gmail.com>
2017-11-30 17:16:56 -0700

    feat(socket.service): begin listening with socket.io

--- src/services/socket.service.ts
+++ src/services/socket.service.ts
@@ -1,13 +1,14 @@
 import { Service } from 'miter';
+import * as http from 'http';
+import * as https from 'https';
+import io = require('socket.io');
 
 @Service()
 export class MiterSocketService {
     constructor() { }
     
-    async start() {
-        
-    }
-    async stop() {
-        
+    async start() { }
+    async listen(webServer: http.Server | https.Server) {
+        io.listen(webServer);
     }
 }
